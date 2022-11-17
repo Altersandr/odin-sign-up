5c04290273b54a0aa0e666fbe6183bfb05f3e0cb
Oleks <archer.opendub@gmail.com>
2022-11-17 15:22:39 +0100

    implemented the matching password check, its basic but the form doesnt require anything else, design responsive enough for smaller or larger desktops, havent tested on other devices or other browsers other than Chrome, will be moving on to the next project

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const pwd = document.getElementById('pwd');
 
+const pwdc = document.getElementById('pwdc');
+
+const form = document.getElementById('form');
+
+pwdc.addEventListener('input', (e)=>{
+    console.log(pwd.value)
+    if(pwdc.value != pwd.value){
+        pwdc.setCustomValidity("The passwords don't match");
+        pwdc.reportValidity();
+    }else{
+        pwdc.setCustomValidity("")
+    }
+})
+
+console.log(pwd.value)
